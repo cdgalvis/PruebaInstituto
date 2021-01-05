a9381ea19d589d8a877259e06ea9179f49df0abf
Cristhian Galvis <cristiangalviscardona@gmail.com>
2021-01-05 14:39:37 -0500

    Se modifico la pantalla de inicio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+         <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
         <title>Laravel</title>
 
         <!-- Fonts -->
@@ -49,7 +59,7 @@
             }
 
             .links > a {
-                color: #636b6f;
+                color: #0A0909;
                 padding: 0 25px;
                 font-size: 13px;
                 font-weight: 600;
@@ -61,39 +71,37 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+
+            /*body {
+                margin: 0; 
+                background-image: url("../storage/fondo.jpg");
+                background-size: cover;
+                
+            }*/
+
+            
+
         </style>
     </head>
-    <body>
+    <body class="fondo-blur">
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
-                <div class="top-right links">
+                <div class="top-right links">                    
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ url('/home') }}" class="btn btn-primary" >Cursos</a>
+
+                        <a href="{{ route('login') }}" class="btn btn-primary" >Login</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a href="{{ route('register') }}" class="btn btn-primary" >Register</a>
                         @endif
                     @endauth
                 </div>
             @endif
-
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                
             </div>
         </div>
     </body>
